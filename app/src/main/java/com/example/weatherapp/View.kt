132b2eedec847dee47efc199f528f7e42bbd9c45
wilmer <wilmercp28@gmail.com>
2023-08-02 17:25:20 -0400

    Build

--- app/src/main/java/com/example/weatherapp/View.kt
+++ app/src/main/java/com/example/weatherapp/View.kt
@@ -3,6 +3,7 @@ package com.example.weatherapp
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -17,17 +18,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
@@ -61,26 +68,64 @@ fun WeatherAppPreview() {
 
 @Composable
 fun WeatherAppUI(viewModel: ViewModel = ViewModel()){
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+    Spacer(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(20.dp)
+            .background(MaterialTheme.colorScheme.primary)
+    )
     Column(modifier = Modifier.fillMaxSize()){
 Box(
     modifier = Modifier
         .background(MaterialTheme.colorScheme.primary)
         .fillMaxSize(),
-
 ) {
-
-    Box(
+    Row(
         modifier = Modifier
-            .size(200.dp)
-            .align(Alignment.TopCenter)
-    ){
-        Image(bitmap = viewModel.WeatherIcon(), contentDescription = "",
+            .height(200.dp)
+            .fillMaxWidth()
+            .align(Alignment.TopCenter),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(
+            text = viewModel.GetTime(),
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+        Image(
+
+            bitmap = viewModel.WeatherIcon(), contentDescription = "",
             modifier = Modifier
-                .fillMaxSize()
+                .size(200.dp),
+            contentScale = ContentScale.Fit
+        )
+        Column(
+            modifier = Modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = viewModel.GetDay(),
+                fontSize = 60.sp,
+                color = MaterialTheme.colorScheme.onPrimary,
+                textAlign = TextAlign.Center
             )
+            Text(
+                text = viewModel.GetMonth(),
+                fontSize = 30.sp,
+                color = MaterialTheme.colorScheme.onPrimary,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+
+
     }
 }
-
     }
 
 }
--- app/src/main/java/com/example/weatherapp/ViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ViewModel.kt
@@ -4,17 +4,41 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 
 class ViewModel: ViewModel(){
+    private var weather = "Cloudy"
+    val calendar = Calendar.getInstance()
     @Composable
     fun WeatherIcon(): ImageBitmap {
         val sun = ImageBitmap.imageResource(R.drawable.sun)
-        val claudy = ImageBitmap.imageResource(R.drawable.cloudy)
+        val cloudy = ImageBitmap.imageResource(R.drawable.cloudy)
         val raining = ImageBitmap.imageResource(R.drawable.raining)
-        return sun
+        val defaultWeather = ImageBitmap.imageResource(R.drawable.defaultweather)
+        when (weather) {
+            "Sunny" -> return sun
+            "Cloudy" -> return cloudy
+            "Raining" -> return raining
+            else -> return defaultWeather
+        }
+
     }
 
+    @Composable
+    fun GetTime(): String {
+        val timeFormat = SimpleDateFormat("hh:mm \na", Locale.getDefault())
 
-
+        return timeFormat.format(calendar.time)
+    }
+    fun GetMonth(): String {
+        val dateFormat = SimpleDateFormat("MM", Locale.getDefault())
+        return dateFormat.format(calendar.time)
+    }
+    fun GetDay(): String {
+        val dateFormat = SimpleDateFormat("dd", Locale.getDefault())
+        return dateFormat.format(calendar.time)
+    }
 }
