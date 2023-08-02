c5d1e4a2446b384642aecab5e67d7c37dee64b92
wilmer <wilmercp28@gmail.com>
2023-08-02 14:07:31 -0400

    Build

--- app/src/main/java/com/example/weatherapp/View.kt
+++ app/src/main/java/com/example/weatherapp/View.kt
@@ -1,27 +1,35 @@
 package com.example.weatherapp
 
 import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.weatherapp.ui.theme.WeatherAppTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class View : ComponentActivity() {
     
@@ -40,46 +48,37 @@ class View : ComponentActivity() {
         }
     }
 }
+
+
 @Preview(showBackground = true)
 @Composable
 fun WeatherAppPreview() {
     WeatherAppTheme {
+
         WeatherAppUI()
     }
 }
 
 @Composable
-fun WeatherAppUI(){
-    val sun: ImageBitmap = painterResource(R.drawable.sun) as ImageBitmap
+fun WeatherAppUI(viewModel: ViewModel = ViewModel()){
     Column(modifier = Modifier.fillMaxSize()){
 Box(
     modifier = Modifier
         .background(MaterialTheme.colorScheme.primary)
-        .height(300.dp)
-        .fillMaxWidth()
+        .fillMaxSize(),
 ) {
+
     Box(
         modifier = Modifier
-            .size(100.dp)
+            .size(200.dp)
+            .align(Alignment.TopCenter)
     ){
-        Image(bitmap = sun, contentDescription = "")
+        Image(bitmap = viewModel.WeatherIcon(), contentDescription = "",
+            modifier = Modifier
+                .fillMaxSize()
+            )
     }
 }
-        Row(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.background)
-                .height(100.dp)
-                .fillMaxWidth()
-        ) {
-
-        }
-        Box(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.secondary)
-                .fillMaxSize()
-        )
-
-
 
     }
 
--- app/src/main/java/com/example/weatherapp/ViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ViewModel.kt
@@ -1,2 +1,20 @@
 package com.example.weatherapp
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.lifecycle.ViewModel
+
+
+class ViewModel: ViewModel(){
+    @Composable
+    fun WeatherIcon(): ImageBitmap {
+        val sun = ImageBitmap.imageResource(R.drawable.sun)
+        val claudy = ImageBitmap.imageResource(R.drawable.cloudy)
+        val raining = ImageBitmap.imageResource(R.drawable.raining)
+        return sun
+    }
+
+
+
+}
